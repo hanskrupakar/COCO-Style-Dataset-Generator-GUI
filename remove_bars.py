7a9d6663bd7a2523d58ea364ccaf8c2d059ed3a5
hanskrupakar <hanskrupakar1994@gmail.com>
2018-03-26 12:02:34 -0400

    fixed-glitches

--- remove_bars.py
+++ remove_bars.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageChops
-import sys, glob, os
+import sys, glob, os, math
 
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
@@ -9,12 +9,40 @@ def trim(im):
     if bbox:
         return im.crop(bbox)
 
+def rotatedRectWithMaxArea(w, h, angle):
+  """
+  Given a rectangle of size wxh that has been rotated by 'angle' (in
+  radians), computes the width and height of the largest possible
+  axis-aligned rectangle (maximal area) within the rotated rectangle.
+  """
+  if w <= 0 or h <= 0:
+    return 0,0
+
+  width_is_longer = w >= h
+  side_long, side_short = (w,h) if width_is_longer else (h,w)
+
+  # since the solutions for angle, -angle and 180-angle are all the same,
+  # if suffices to look at the first quadrant and the absolute values of sin,cos:
+  sin_a, cos_a = abs(math.sin(angle)), abs(math.cos(angle))
+  if side_short <= 2.*sin_a*cos_a*side_long or abs(sin_a-cos_a) < 1e-10:
+    # half constrained case: two crop corners touch the longer side,
+    #   the other two corners are on the mid-line parallel to the longer line
+    x = 0.5*side_short
+    wr,hr = (x/sin_a,x/cos_a) if width_is_longer else (x/cos_a,x/sin_a)
+  else:
+    # fully constrained case: crop touches all 4 sides
+    cos_2a = cos_a*cos_a - sin_a*sin_a
+    wr,hr = (w*cos_a - h*sin_a)/cos_2a, (h*cos_a - w*sin_a)/cos_2a
+
+  return wr,hr
+
 if __name__=='__main__':
 
     for f in glob.glob(os.path.join(sys.argv[1], '*.jpg')):
 
         im = Image.open(f)
-        im = trim(im).resize((1920, 1080), Image.ANTIALIAS)
+        im = trim(im)
+        #print (rotatedRectWithMaxArea(1920, 1080, 90))
         im.save(f)
 
 

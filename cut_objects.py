bb10f0ca10f50be10668162509b9b6c0cf9ef7eb
hanskrupakar <hanskrupakar1994@gmail.com>
2018-03-27 21:56:17 -0400

    occlusions-study

--- cut_objects.py
+++ cut_objects.py
@@ -8,24 +8,25 @@ from matplotlib.path import Path
 
 class Occlusion_Generator_Bbox(object):
 
-    def __init__(self, json_file):
-        
+    def __init__(self, json_file, bg_path):
+    
         self.dataset = json.load(open(json_file))
-        
-        imgToAnns = defaultdict(list)
+        self.imgToAnns = defaultdict(list)
         for ann in self.dataset['annotations']:
-            imgToAnns[ann['image_id']].append(ann)
+            self.imgToAnns[ann['image_id']].append(ann)
         
-        objToAnns = [[] for _ in range(len(self.dataset['classes'])+1)]
-        for index in imgToAnns:
-            for obj in imgToAnns[index]:
-                objToAnns[obj['category_id']].append({'image': obj['image_id'], 'bbox':obj['bbox']})
+        self.objToAnns = [[] for _ in range(len(self.dataset['classes'])+1)]
+        for index in self.imgToAnns:
+            for obj in self.imgToAnns[index]:
+                self.objToAnns[obj['category_id']].append({'image': obj['image_id'], 'bbox':obj['bbox']})
         
-
+        self.img = Image.open(bg_path).convert("RGBA")
+        self.mask_img = Image.new('L', self.img.size, 0)
+        
     def cut_bbox(self, rect): # Takes a bounding box of the form [x_min, y_min, x_max, y_max] and splits it in 2 based on a sine wave and returns 2 PIL polygons
 
         x = np.linspace(rect[0], rect[2], num=50)
-        y = (rect[3]+rect[1])/2 + 15*np.sin(x/(500/np.pi/2))
+        y = (rect[3]+rect[1])/2 + 15*np.sin(x/(rect[3]/np.pi/2))
         
         x1 = np.concatenate((x, np.array([rect[2], rect[0]])))
         y1 = np.concatenate((y, np.array([rect[3], rect[3]])))
@@ -38,15 +39,43 @@ class Occlusion_Generator_Bbox(object):
         
         return random.choice([poly1, poly2])
     
-    def create_canvas(self, bg_path):
+    def add_objects(self, n_objs):
         
-        im = Image.open(bg_path).convert("RGBA")
-        
-        # Create mask
-        img = Image.new('L', (width, height), 0)
-        ImageDraw.Draw(img).polygon(polygon, outline=1, fill=1)
-        mask = numpy.array(img)
-    
+        for _ in range(n_objs):
+
+            c1 = random.randint(1, len(self.objToAnns)-1)
+            c2 = random.randint(0, len(self.objToAnns[c1])-1)
+
+            obj = Image.open(next(item for item in self.dataset['images'] if item["id"] == self.objToAnns[c1][c2]['image'])['file_name'])
+            obj_bbox = self.objToAnns[c1][c2]['bbox']
+            obj_bbox = (obj_bbox[2], obj_bbox[3], obj_bbox[0], obj_bbox[1])
+            
+            obj_mask = Image.new('L', obj.size, 0)      
+            random_occ = self.cut_bbox(obj_bbox)   
+            ImageDraw.Draw(obj_mask).polygon(random_occ, outline=255, fill=255)
+            
+            obj = obj.crop(obj_bbox)
+            obj_mask = obj_mask.crop(obj_bbox)
+
+            obj = obj.resize(np.array(np.array(obj.size)*1.5, dtype=int))
+            obj_mask = obj_mask.resize(np.array(np.array(obj_mask.size)*1.5, dtype=int))
+
+            done_flag = False
+
+            while not done_flag:
+
+                randx = random.randint(0, self.img.size[0]-obj.size[0]-2)
+                randy = random.randint(0, self.img.size[1]-obj.size[1]-2)
+
+                temp_mask = self.mask_img.copy()
+                temp_mask.paste(Image.new('L', obj_mask.size, 0), (randx, randy))
+
+                if (temp_mask == self.mask_img):
+                    self.img.paste(obj, (randx, randy), obj_mask)
+                    self.mask_img.paste(obj_mask, (randx, randy))
+                    done_flag=True
+        self.img.show()
+
     '''
     def generate_images(num, path):
             
@@ -117,5 +146,5 @@ class Occlusion_Generator_Bbox(object):
     '''
 
 if __name__=='__main__':
-    occ = Occlusion_Generator_Bbox()
-    print (occ.cut_bbox([50, 100, 500, 500]))
+    occ = Occlusion_Generator_Bbox('../pascal_dataset.json', 'bg.jpg')
+    occ.add_objects(10)

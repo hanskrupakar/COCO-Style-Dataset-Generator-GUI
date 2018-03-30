45f3c9b41b0088efdc0349afd95123c599bf5c62
hanskrupakar <hanskrupakar1994@gmail.com>
2018-03-30 00:14:21 -0400

    api-changes

--- remove_bars.py
+++ remove_bars.py
@@ -9,23 +9,6 @@ def trim(im):
         if bbox:
             return im.crop(bbox)
 
-def rotatedRectWithMaxArea(w, h, angle):
-
-    if w <= 0 or h <= 0:
-        return 0,0
-
-    max_side, min_side = (w,h) if w >= h else (h,w)
-    sin_a, cos_a = abs(math.sin(angle)), abs(math.cos(angle))
-    
-    if min_side <= 2.*sin_a*cos_a*side_long or abs(sin_a-cos_a) < 1e-10:
-        x = 0.5*min_side
-        wr,hr = (x/sin_a,x/cos_a) if w >= h else (x/cos_a,x/sin_a)
-    else:
-        cos_2a = cos_a*cos_a - sin_a*sin_a
-        wr,hr = (w*cos_a - h*sin_a)/cos_2a, (h*cos_a - w*sin_a)/cos_2a
-
-    return wr,hr
-
 if __name__=='__main__':
 
         ap = argparse.ArgumentParser()
@@ -35,5 +18,4 @@ if __name__=='__main__':
         for f in glob.glob(os.path.join(args.images_dir, '*')):
                 im = Image.open(f)
                 im = trim(im)
-                #print (rotatedRectWithMaxArea(1920, 1080, 90))
                 im.save(f)

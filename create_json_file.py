7305905414a0ca23a8e63b358f7f21c6d12f865c
hanskrupakar <hanskrupakar1994@gmail.com>
2018-04-05 01:04:43 -0400

    augment-dataset-with-flipped-images

--- create_json_file.py
+++ create_json_file.py
@@ -5,10 +5,7 @@ import os
 import numpy as np
 import json
 
-from json import encoder
-encoder.FLOAT_REPR = lambda o: format(o, '.2f')
-
-import cv2
+from PIL import Image
 from segment import COCO_dataset_generator as cocogen
 
 if __name__=='__main__':
@@ -25,11 +22,23 @@ if __name__=='__main__':
         
     images, anns = [], []
 
-    for i, f in enumerate(sorted(glob.glob(os.path.join(os.path.abspath(args["image_dir"]), '*.'+args['type'])))):
-        img = cv2.imread(f)
-        height, width, _ = img.shape
-        dic = {'file_name': f, 'id': i, 'height': height, 'width': width}
-        images.append(dic)
+    num_imgs = len(glob.glob(os.path.join(args["image_dir"], '*.txt')))
+    i=0
+    for f in sorted(glob.glob(os.path.join(os.path.abspath(args["image_dir"]), '*.'+args['type']))):
+        
+        if ('f.'+args['type'] not in f):
+	        img = Image.open(f)
+	        width, height = img.size
+	        dic = {'file_name': f, 'id': i, 'height': height, 'width': width}
+	        images.append(dic)
+
+	        #Add flipped image 
+	        if (not os.path.exists(f[:-4]+'f.'+args['type']) and 'f.'+args['type'] not in f):
+	            f_img = img.transpose(Image.FLIP_LEFT_RIGHT)
+	            f_img.save(f[:-4]+'f.jpg')
+	        dic = {'file_name': f[:-4]+'f.jpg', 'id': num_imgs+i, 'height': height, 'width': width}
+	        images.append(dic)
+	        i+=1
 
     ann_index = 0
     for i, f in enumerate(sorted(glob.glob(os.path.join(os.path.abspath(args['image_dir']), '*.txt')))):
@@ -37,8 +46,11 @@ if __name__=='__main__':
         ptr = 0
         with open(f, 'r') as g:
             s = g.read()
-        s = s.split('\n')[4:-1]
+        s = s.split('\n')[2:-1]
         
+        width, height = [int(x) for x in s[0].split(' ')]
+        s = s[2:]
+
         while(ptr<len(s)):
             
             cat_id = classes.index(s[ptr])+1
@@ -56,7 +68,7 @@ if __name__=='__main__':
             for p in poly:
                 points = np.reshape(np.array(p), (int(len(p)/2), 2))
                 
-                if x1 is None:
+                if x1 is None: 
                     x1, y1 = points.min(0)
                     x2, y2 = points.max(0)
                 else:
@@ -73,6 +85,15 @@ if __name__=='__main__':
             dic2 = {'segmentation': poly, 'area': area, 'iscrowd':0, 'image_id':i, 'bbox':bbox, 'category_id': cat_id, 'id': ann_index}
             ann_index+=1
             ptr+=4
+            anns.append(dic2)
+          
+            # Add flipped annotations as well to improve training context
+            poly2 = [[round(width-p-1, 2) if i%2==0 else p for i,p in enumerate(polygon)] for polygon in poly]
+            
+            bbox[0] = round(width - bbox[0] - 1, 2)
+            bbox[2] = round(width - bbox[2] - 1, 2)
+            dic2 = {'segmentation': poly2, 'area': area, 'iscrowd':0, 'image_id':num_imgs+i, 'bbox':bbox, 'category_id': cat_id, 'id': ann_index}
+            ann_index+=1
             anns.append(dic2)
 
     data = {'images':images, 'annotations':anns, 'categories':[], 'classes': classes}

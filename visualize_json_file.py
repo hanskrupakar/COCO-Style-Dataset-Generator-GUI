ff63639cbb0f38ac334d7bc19303833ef874a08f
hanskrupakar <hansk@nyu.edu>
2018-12-22 17:50:24 -0500

    added-mask-visualization

--- visualize_json_file.py
+++ visualize_json_file.py
@@ -1,6 +1,8 @@
 import json
 import cv2
 import argparse
+import os
+import numpy as np
 
 if __name__=='__main__':
     
@@ -18,14 +20,21 @@ if __name__=='__main__':
     
     print ("Dataset contains %d images, %d objects!"%(len(images), len(annotations)))
     for img in images:
-        anns = [ann for ann in annotations if ann["image_id"]==img["id"]]
-        image_cv2 = cv2.imread(img["file_name"])
-        for ann in anns:
-            s = [int(x) for x in ann['bbox']]
-            cv2.rectangle(image_cv2, (s[0], s[1]), (s[2], s[3]), (0,0,0), 2)
-            cv2.putText(image_cv2, classes[ann['category_id']-1], (s[0]-10, s[1]+10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
-        cv2.imshow('frame', image_cv2)
-        q = cv2.waitKey()
+        if os.path.exists(img['file_name']):
+            anns = [ann for ann in annotations if ann["image_id"]==img["id"]]
+            image_cv2 = cv2.imread(img["file_name"])
+            ann_img = image_cv2.copy()
+            for ann in anns:
+                s = [int(x) for x in ann['bbox']]
+                seg = np.array(ann['segmentation'][0])
+                x, y = seg[range(0, len(seg)-1, 2)], seg[range(1, len(seg), 2)]
+                seg2d = [[xi, yi] for xi, yi in zip(x,y)]
+                cv2.fillPoly(ann_img, np.array([seg2d], dtype = 'int32'), (0, 255, 0))
+                cv2.rectangle(image_cv2, (s[0], s[1]), (s[2], s[3]), (0,0,0), 2)
+                cv2.putText(image_cv2, classes[ann['category_id']-1], (s[0]-10, s[1]+10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+            image_cv2 = cv2.addWeighted(ann_img,0.25,image_cv2,0.75,0)
+            cv2.imshow('frame', image_cv2)
+            q = cv2.waitKey()
         
-        if q == 113: # if q == 'q'
-            exit()
+            if q == 113: # if q == 'q'
+                exit()

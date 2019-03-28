56cdd247c1e37a09da52a7b29538bc9608cea9df
hanskrupakar <hanskrupakar1994@gmail.com>
2019-03-28 07:36:15 -0400

    added-non-gui-save-option-for-testing-on-server

--- visualize_json_file.py
+++ visualize_json_file.py
@@ -8,10 +8,13 @@ if __name__=='__main__':
     
     ap = argparse.ArgumentParser()
     ap.add_argument('json_file', help="Path to the JSON dataset file to visualize")
+    ap.add_argument('--save', help='Save a few results to disk to accommodate non-display environments', action='store_true')
     args = ap.parse_args()
     
+    '''
     cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
-    
+    '''
+
     with open(args.json_file, 'r') as f:    
         obj = json.loads(f.read())
     
@@ -19,10 +22,12 @@ if __name__=='__main__':
     classes = obj["classes"]
     
     print ("Dataset contains %d images, %d objects!"%(len(images), len(annotations)))
-    for img in images:
-        if os.path.exists(img['file_name']):
+    
+    for idx, img in enumerate(images):
+        imgpath = os.path.join(os.path.dirname(args.json_file), img['file_name'])
+        if os.path.exists(imgpath):
             anns = [ann for ann in annotations if ann["image_id"]==img["id"]]
-            image_cv2 = cv2.imread(img["file_name"])
+            image_cv2 = cv2.imread(imgpath)
             ann_img = image_cv2.copy()
             for ann in anns:
                 s = [int(x) for x in ann['bbox']]
@@ -33,8 +38,16 @@ if __name__=='__main__':
                 cv2.rectangle(image_cv2, (s[0], s[1]), (s[2], s[3]), (0,0,0), 2)
                 cv2.putText(image_cv2, classes[ann['category_id']-1], (s[0]-10, s[1]+10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
             image_cv2 = cv2.addWeighted(ann_img,0.25,image_cv2,0.75,0)
-            cv2.imshow('frame', image_cv2)
-            q = cv2.waitKey()
-        
+
+            if not args.save:
+                cv2.imshow('frame', image_cv2)
+                q = cv2.waitKey()
+            
+            else:
+                cv2.imwrite('sample%d.jpg'%(idx), image_cv2)
+                q = 10    
+                if idx > 5:
+                    q = 113
+
             if q == 113: # if q == 'q'
                 exit()
